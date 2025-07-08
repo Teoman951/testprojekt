c61b9099ecb2efe5044a5290cf9378cc8e8e4dd9
reida2 <ridaucb@gmail.com>
2025-07-08 23:46:58 +0200

    vite.config.js für docker

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,            // = 0.0.0.0
+    port: 5173,
     proxy: {
-      '/api': 'http://localhost:3001', // ← Passe den Port an deinen Backend-Port an
-    },
-  },
-});
+      '/api': {
+        target: 'http://backend:3001',  // Service-Name aus Compose!
+        changeOrigin: true,
+      }
+    }
+  }
+})
